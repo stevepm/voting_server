4c077e1a1032bbc22055e17aa3565def403520d8
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-12-05 14:57:43 -0800

    next function sets the pair that should be voted on

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,10 +1,12 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
+
   describe('setEntries', () => {
+
     it('adds the entries to the state', () => {
       const state = Map();
       const entries = List.of('Trainspotting', '28 Days Later');
@@ -13,5 +15,38 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
   });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
+
+  });
+
+  describe('vote', () => {
+
+
+  });
+
 });
